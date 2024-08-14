ca533d057efab19f452603a9bff638e4720873d6
richardcris <Richardalbuquerque56@gmail.com>
2024-08-14 01:21:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -458,11 +458,27 @@ document.addEventListener("DOMContentLoaded", function() {
 
 // script.js
 
-document.addEventListener('DOMContentLoaded', function() {
-    const toggleButton = document.getElementById('toggleButton');
-    const sidebar = document.querySelector('nav.sidebar');
+const body = document.querySelector("body"),
+  sidebar = body.querySelector("nav"),
+  toggle = body.querySelector(".toggle"),
+  searchBtn = body.querySelector(".search-box"),
+  modeSwitch = body.querySelector(".toggle-switch"),
+  modeText = body.querySelector(".mode-text");
 
-    toggleButton.addEventListener('click', function() {
-        sidebar.classList.toggle('active');
-    });
+toggle.addEventListener("click", () => {
+  sidebar.classList.toggle("close");
+});
+
+searchBtn.addEventListener("click", () => {
+  sidebar.classList.remove("close");
+});
+
+modeSwitch.addEventListener("click", () => {
+  body.classList.toggle("dark");
+
+  if (body.classList.contains("dark")) {
+    modeText.innerText = "Light mode";
+  } else {
+    modeText.innerText = "Dark mode";
+  }
 });
